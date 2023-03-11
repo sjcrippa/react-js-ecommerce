d0fc5d048db54350721a205a28977118bc4f3cff
Santiago Crippa <santicrippa@gmail.com>
2023-03-11 16:21:39 -0300

    cart adding

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,8 +2,10 @@ import React from "react"
 export const CartWidget = ({ cantCarrito }) => {
     return (
         <>
-            <button className="btn btn-dark btnCart">Carrito</button>
+            <button className="btn btn-dark"><i className="fa-solid fa-cart-shopping"></i>
             <p>{cantCarrito}</p>
+            </button>
+            
         </>
     )
 }
