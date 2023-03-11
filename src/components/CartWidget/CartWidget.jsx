d742b69f36b8ca91ad63dd46aaac688629e15f52
Santiago Crippa <santicrippa@gmail.com>
2023-03-11 16:39:23 -0300

    more adds to cart

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,7 @@ import React from "react"
 export const CartWidget = ({ cantCarrito }) => {
     return (
         <>
-            <button className="btn btn-dark"><i className="fa-solid fa-cart-shopping"></i>
+            <button className="btn"><i className="fa-solid fa-cart-shopping"></i>
             <p>{cantCarrito}</p>
             </button>
             
--- src/components/NavBarComponent/NavBarComp.jsx
+++ src/components/NavBarComponent/NavBarComp.jsx
@@ -8,7 +8,7 @@ export const NavBarComp = () => {
 
                 <a className="navbar-brand" href="#">El Garete</a>
 
-                <CartWidget />
+                <CartWidget cantCarrito={10} />
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
